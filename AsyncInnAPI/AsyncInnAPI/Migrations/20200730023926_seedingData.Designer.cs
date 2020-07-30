96291eb1296e76773557407187304c2cdddb745f
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-07-29 19:40:08 -0700

    data seeded to the database

--- AsyncInnAPI/AsyncInnAPI/Migrations/20200730023926_seedingData.Designer.cs
+++ AsyncInnAPI/AsyncInnAPI/Migrations/20200730023926_seedingData.Designer.cs
@@ -3,14 +3,16 @@ using AsyncInnAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsyncInnAPI.Migrations
 {
     [DbContext(typeof(AsyncInnDbContext))]
-    partial class AsyncInnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200730023926_seedingData")]
+    partial class seedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
