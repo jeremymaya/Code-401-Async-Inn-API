7bb486af74bbe113c9846f191369c4ff9a8a4017
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-07-30 15:00:41 -0700

    repository design for HotelsController completed

---
+++ AsyncInnAPI/AsyncInnAPI/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncInnAPI.Models.Interfaces
+{
+    public interface IHotelManager
+    {
+        Task<Hotel> CreateHotel(Hotel hotel);
+        Task DeleteHotel(int id);
+        Task<List<Hotel>> GetHotels();
+        Task<Hotel> GetHotel(int id);
+        Task UpdateHotel(Hotel hotel);
+    }
+}
--- AsyncInnAPI/AsyncInnAPI/Startup.cs
+++ AsyncInnAPI/AsyncInnAPI/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AsyncInnAPI.Data;
+using AsyncInnAPI.Models.Interfaces;
+using AsyncInnAPI.Models.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -28,7 +30,10 @@ namespace AsyncInnAPI
         {
             // Enables the use of MVC controllers
             services.AddMvc();
+
             services.AddDbContext<AsyncInnDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IHotelManager, HotelManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
